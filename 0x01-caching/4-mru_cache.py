3fdf431b62d14013ef5b86b039bb031c528da326
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-08-21 23:09:29 +0000

    mru-caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+    class BasicCache that inherits from BaseCaching and
+    is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+        __summary__
+    """
+
+    def __init__(self):
+        """
+            __summary__
+        """
+        super().__init__()
+        self.usedKeys = []
+
+    def put(self, key, item):
+        """__summary__
+
+            Args:
+                key
+                item
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+
+        if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+            remove = self.usedKeys.pop(-2)
+            del self.cache_data[remove]
+            print("DISCARD: {:s}".format(remove))
+
+    def get(self, key):
+        """
+            return the value in self.cache_data linked to key
+
+            Args:
+                key(__type__):_description_
+        """
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None
