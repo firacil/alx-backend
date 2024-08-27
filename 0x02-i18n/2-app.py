924f1d05c21057018c4a30c454fa67414bca45ba
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-08-27 18:21:29 +0000

    getlocale

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""module to insantiate babel object"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """class to configure available languages"""
+    LANGUAGES = ['en', 'fr']
+
+    # configure babels default locale
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """return the bestmatch clients preferred langs
+    and supported one
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """return babel object locale"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
